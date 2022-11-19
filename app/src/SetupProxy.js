b36f0a30335cbc317517d4c777ac1dbee88d8299
Mahesh Kasbe <maheshkasbe010@gmail.com>
2022-11-19 20:58:45 +0530

    last commit

--- app/src/SetupProxy.js
+++ app/src/SetupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = app => {
   app.use(
     createProxyMiddleware({
-      target: 'https://kubernetes.default.svc',
+      target: 'http://127.0.0.1:8001',
       changeOrigin: true,
     })
   );
